f78bd5cd69200796b32cea83c0dd437899b5edf3
Daniel Ramirez <danielramire1996@gmail.com>
2023-01-22 17:44:09 -0500

    evento_login

--- ProyectoX/src/main.jsx
+++ ProyectoX/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import"./Styles/estilos.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
