032ba442a3d7ba0abbf294584954f9d8e108e1c7
Pablo-N <pablov1219@gmail.com>
2023-01-31 08:21:01 -0500

    ajustada la api para que se opueda llamar directamente localhost desde fetch

--- ProyectoX/vite.config.js
+++ ProyectoX/vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
   plugins: [react()],
 })
