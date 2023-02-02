827ce48218a9028135fe1db5e3542bb0734154b5
Pablo-N <pablov1219@gmail.com>
2023-02-02 16:33:40 -0500

    Ajustado cross origin ciudades

--- ProyectoX_API/src/main/java/DH/Integrador/Grupo6/ProyectoX_API/controller/CiudadController.java
+++ ProyectoX_API/src/main/java/DH/Integrador/Grupo6/ProyectoX_API/controller/CiudadController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/ciudades")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","http://127.0.0.1:5173/"})
 public class CiudadController {
 
     private CiudadService ciudadService;
