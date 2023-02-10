77c09cd4fa87a2c244d26923f019665816855fc0
Mauro9735 <mauriciovilladiego1999@gmail.com>
2023-02-10 16:07:13 -0500

    Arreglando Inconos

--- ProyectoX/src/Contenedor/TemplateContenedor.jsx
+++ ProyectoX/src/Contenedor/TemplateContenedor.jsx
@@ -1,70 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Styles from "../Styles/Template.css"
+import "../Styles/Template.css"
+import { useParams } from "react-router";
 
 
 
 const TemplateContenedor = () => {
+
+
+
+    const datos = useParams();
+    const [datosProducto, setDatosProducto] = useState([]);
+
+    useEffect(() =>{
+        async function getProduct(){
+            try{
+              const response = await fetch(`http://localhost:8080/producto/${datos.id}`);
+              const data = await response.json();
+              console.log(data);
+              setDatosProducto(data);
+            }catch(e){
+              console.log("Error");
+              console.log(e);
+            }
+          }
+          getProduct();
+        }, [datos.id]);
+
+
+        {console.log(datosProducto)}
+
 
     return (
 
         <div className="contenedor">
             <header>
             <div className="categoryProduct">
-                <p>  Tipo alojamiento </p>
-                <h3> Nombre alojamiento </h3>
-                <div className="arrowLeft"> <FontAwesomeIcon icon={faArrowLeft}/> </div>
+                {/* <p></p> */}
+                <span style={{color:"#000",marginRight:"15px"}}> {datosProducto.tituloDescripcion} </span>
+                <span className="arrowLeft"> <FontAwesomeIcon icon={faArrowLeft}/></span> 
                 
             </div>
 
             <div className="ubication">
-                <h4> Datos de ubicacion alojamiento  </h4>
-                <p> Calificacion </p>
+                <span> Medellín, Colombia </span>
+                <p> Calificacion: {datosProducto.puntaje} estrellas </p>
             </div>
             </header>
 
             <body>
                 <div className="grilla-imagenes">
                     <p> Grilla Imagenes </p>
+                    {/* {datosProducto.imagenes.map((elemento) => {<img href={elemento.urlImagen}/>})} */}
                 </div>
 
                 <div className="descripcion">
-                    <h3> Lorem Ipsum </h3>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum.
+                    <h3> Descripción: </h3>
+                    <p> {datosProducto.contenidoDescripcion}
                     </p>
                 </div>
 
                 <div className="servicios">
                     <h3> ¿Qué ofrece este lugar? </h3>
-                    <div className="logos">
+                    <p>Libre de Humo,Piscina, Wifi, Desayuno hasta las 10 AM</p>
+                    
+                    {/* <div className="logos">
 
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="politica-producto">
                     <h3> Qué tienes que saber </h3>
                     <span className="normas de la casa">
-                        <h5> Normas de la casa </h5>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
-                        <li>Lorem Ipsum</li>
+                        <h5> Normas: </h5>
+                        <ol>
+                        <li>No hacer fiestas</li>
+                        <li>No brincar en las camas</li>
+                        <li>No meter en la habotación más de 10 personas</li>
+                        </ol>
                     </span>
-                    <span className="seguridad-salud">
+                    {/* <span className="seguridad-salud">
                         <h5> Seguridad y salud</h5>
                         <li>Lorem Ipsum</li>
                         <li>Lorem Ipsum</li>
                         <li>Lorem Ipsum</li>
-                    </span>
-                    <span>
+                    </span> */}
+                    {/* <span>
                         <h5> Politica de cancelación </h5>
-                    </span>
+                    </span> */}
 
 
                 </div>
