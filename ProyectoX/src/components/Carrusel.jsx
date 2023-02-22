75c8b830914a3141350ce5cb649128366a99eea0
Santiago <santibeta133@gmail.com>
2023-02-22 15:15:19 -0500

    solicitud de pull

--- ProyectoX/src/components/Carrusel.jsx
+++ ProyectoX/src/components/Carrusel.jsx
@@ -80,61 +80,76 @@ export default Carrusel;
 
 
 
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'react-router';
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+// import imagenes from "../data/imagesDetails.json";
 // import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
-// import { Link } from 'react-router-dom';
 // import "swiper/css";
 // import "swiper/css/navigation";
 // import "swiper/css/pagination";
-// import "swiper/css/effect-flip";
-// import "swiper/css/effect-fade";
+// import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+// import CarouselImg from "./CarouselImg";
+// import Gallery from "./Gallery";
+// import { useParams, Link } from "react-router-dom";
 
+// function ImagesProduct({product}) {
+//   const [visibleModal, setVisibleModal] = useState(false);
+  // const [product, setProduct] = useState()
 
-// function Carrusel() {
-//   const datos = useParams();
-//   const [imagenes, setImagenes] = useState([]);
+  // const { id } = useParams();
 
-//   console.log(datos.id);
+  // async function getImg() {
+    // const data = await fetch(`http://localhost:8080/product/${id}`)
+    // const imgs = await data.json()
+    // setProduct(imgs)
+  // }
 
-//   useEffect(() => {
-//     const pedidoImagenes = async() => {
-//       try {
-//         const pedidoFetch = await fetch(`http://localhost:8080/producto/${datos.id}`);
-//         const respuesta = await pedidoFetch.json();
-//         setImagenes(respuesta);
-//       } catch (error) {
-//         console.log(error);
-//       } 
-//     }
+  // useEffect(() => {
+    // getImg()
+  // },[])
 
-//     console.log("Estas son las imagenes carrusel");
-//     console.log(imagenes, 'IMAGENES');
+//   function handleModal() {
+//     setVisibleModal(!visibleModal);
+//   }
 
-//     pedidoImagenes();
-//   }, [datos.id]);
-
-//   console.log(imagenes);
 //   return (
-//     <div className="carrusel swiper-container">
-//       <Link to={`/producto/${datos.id}`}>
-//         <button className='Atras'>←</button>      
-//       </Link>
-//       <Swiper className="swiper-wrapper" navigation={true}
-//         pagination={true}
-//         mousewheel={true}
-//         keyboard={true}
-//         modules={[Navigation, Pagination, Mousewheel, Keyboard]}>
-
-//         {imagenes.imagenes && imagenes.imagenes.map((image) => (
-//           <SwiperSlide className="swiper-slide" key={image.id}>            
-//             <img src={image.urlImagen} alt="imagenCarrusel" />
-//           </SwiperSlide>          
-//         ))}
-//       </Swiper>
-//     </div>
+//     <>
+//       <div className="gallery-products">
+//         <div className="gallery-products-main">
+//           <img
+//             src={product && product.image.filter((img) =>
+//               img.title.includes("Main")
+//             )[0]?.url}
+//             alt="car"
+//           />
+//         </div>
+//         <div className="gallery-products-mains">
+//           {/* {product &&  imagenes.map((image) => ( */}
+//             {/* <div key={image.url}> */}
+//               {/* <img src={image.url} alt="imagen" /> */}
+//             {/* </div> */}
+//           {/* ))} */}
+//           {product && product.image.filter((img) => 
+//             !img.title.includes("Main")
+//           ).slice(0,4).map(({url}) => (
+//             <img src={url} alt="imagen" />
+//           ))}
+//           <div className="gallery-products-imgs">
+//             <button onClick={handleModal}>View more...</button>
+//           </div>
+//         </div>
+//       </div>
+//       <div className={visibleModal ? "modal" : "view-modal"}>
+//         <div className="buttonModal">
+//           <button onClick={handleModal}>X</button>
+//         </div>
+//         <CarouselImg imgs={product?.image}></CarouselImg>
+//       </div>
+//       <div className="gallery-imgs">
+//         <Gallery imgProducts={product?.image}></Gallery>
+//       </div>
+//     </>
 //   );
-// };
+// }
 
-// export default Carrusel;
+// export default ImagesProduct
--- ProyectoX/src/components/Galeria.jsx
+++ ProyectoX/src/components/Galeria.jsx
@@ -1,59 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import '../Styles/Template.css'
 
 function Galeria() {
-
   const datos = useParams();
-  const [imagenes, setImagenes] = useState([])
-  
+  const [imagenes, setImagenes] = useState([]);
+  const {id} = useParams();
 
- 
-  
+  const navigate = useNavigate();
+
   useEffect(() => {
-    const pedidoImagenes = async() => {
+    const pedidoImagenes = async () => {
       try {
-      const pedidoFetch = await fetch(`http://localhost:8080/producto/${datos.id}`);
-      const respuesta = await pedidoFetch.json();
-      setImagenes(respuesta)
-      // console.log("Estas son las imagenes");
-      // console.log(imagenes);
-        
+        const pedidoFetch = await fetch(`http://localhost:8080/producto/${datos.id}`);
+        const respuesta = await pedidoFetch.json();
+        setImagenes(respuesta);
       } catch (error) {
         console.log(error);
-      } 
-    }
+      }
+    };
 
-    pedidoImagenes() 
-  },[datos.id])
-console.log(imagenes);
-imagenes?.imagenes?.sort((a,b) => a.id - b.id)
-
+    pedidoImagenes();
+  }, [datos.id]);
 
   return (
-      <div className="galeria">
-
-          <div className='imgPrincipal'>
-            <img src={imagenes.imagenes && imagenes.imagenes[0].urlImagen} id="imgPrimera" />
-          </div>
-
-        <div className='img4'>
-          { imagenes.imagenes?.slice(1, 5).map((image) => (
-          <div className='contenedorImg'>
-            <img key={image.id} src={image.urlImagen} alt="Imagen" />
-          </div>
-          ))}
-
-          <div className='botonGrilla'>
-            <Link to={`/carrusel/${datos.id}`}>
-              <button className="verMas">Ver más</button>
-            </Link>
-          </div>
-
-        </div>
-        
+    <div className="galeria">
+      <div className="imgPrincipal">
+        <img src={imagenes.imagenes && imagenes.imagenes[0].urlImagen} id="imgPrimera" />
       </div>
+
+      <div className="img4">
+        {imagenes.imagenes?.slice(1, 5).map((image) => (
+          <div className="contenedorImg" key={image.id}>
+            <img src={image.urlImagen} alt="Imagen" />
+          </div>
+        ))}
+
+        <div className="botonGrilla">
+          <button className="verMas" onClick={ () => navigate(`/carrusel/${id}`)}>Ver más</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -61,47 +48,61 @@ export default Galeria;
 
 
 // import React, { useContext, useEffect, useState } from 'react';
-// import { useHistory } from 'history'; 
 // import { useParams } from 'react-router';
+// import { Link } from 'react-router-dom';
 // import '../Styles/Template.css'
 
 // function Galeria() {
-//   const datos = useParams();
-//   const [imagenes, setImagenes] = useState([]);
-//   const history = useHistory();
 
+//   const datos = useParams();
+//   const [imagenes, setImagenes] = useState([])
+  
+
+ 
+  
 //   useEffect(() => {
-//     const pedidoImagenes = async () => {
+//     const pedidoImagenes = async() => {
 //       try {
-//         const pedidoFetch = await fetch(`http://localhost:8080/producto/${datos.id}`);
-//         const respuesta = await pedidoFetch.json();
-//         setImagenes(respuesta);
+//       const pedidoFetch = await fetch(`http://localhost:8080/producto/${datos.id}`);
+//       const respuesta = await pedidoFetch.json();
+//       setImagenes(respuesta)
+//       // console.log("Estas son las imagenes");
+//       // console.log(imagenes);
+        
 //       } catch (error) {
 //         console.log(error);
-//       }
-//     };
+//       } 
+//     }
 
-//     pedidoImagenes();
-//   }, [datos.id]);
+//     pedidoImagenes() 
+//   },[datos.id])
+// console.log(imagenes);
+// imagenes?.imagenes?.sort((a,b) => a.id - b.id)
+
 
 //   return (
-//     <div className="galeria">
-//       <div className="imgPrincipal">
-//         <img src={imagenes.imagenes && imagenes.imagenes[0].urlImagen} id="imgPrimera" />
-//       </div>
+//       <div className="galeria">
 
-//       <div className="img4">
-//         {imagenes.imagenes?.slice(1, 5).map((image) => (
-//           <div className="contenedorImg" key={image.id}>
-//             <img src={image.urlImagen} alt="Imagen" />
+//           <div className='imgPrincipal'>
+//             <img src={imagenes.imagenes && imagenes.imagenes[0].urlImagen} id="imgPrimera" />
 //           </div>
-//         ))}
 
-//         <div className="botonGrilla">
-//           <button className="verMas" onClick={() => history.push(`/carrusel/${datos.id}`)}>Ver más</button>
+//         <div className='img4'>
+//           { imagenes.imagenes?.slice(1, 5).map((image) => (
+//           <div className='contenedorImg'>
+//             <img key={image.id} src={image.urlImagen} alt="Imagen" />
+//           </div>
+//           ))}
+
+//           <div className='botonGrilla'>
+//             <Link to={`/carrusel/${datos.id}`}>
+//               <button className="verMas">Ver más</button>
+//             </Link>
+//           </div>
+
 //         </div>
+        
 //       </div>
-//     </div>
 //   );
 // };
 
