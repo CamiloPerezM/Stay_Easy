fa5dbfe89cfb739b8c85bab711b9c6cdc77686c3
Mauro9735 <mauriciovilladiego1999@gmail.com>
2023-02-21 21:49:12 -0500

    Terminando el template de reserva

--- ProyectoX/src/App.jsx
+++ ProyectoX/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
           <Route path="/producto/:id" element={<TemplateContenedor />} />
           <Route path='/producto/ciudad/:id' element={<FilterCity/>}/>
           <Route path="/reserve" element={<ReserveTemplate/>}/>
-          <Route path="/reserva" element={<TemplateReserva/>}/>
+          <Route path="/producto/:id/reserva" element={<TemplateReserva/>}/>
 
           <Route path="/carrusel/:id" element={<Carrusel/>} />
         </Routes>
