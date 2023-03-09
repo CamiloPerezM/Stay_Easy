5d7324f0038e6171f4cb21863a49442a531d2f3e
SilviaG <silviguardo15@gmail.com>
2023-03-08 22:41:53 -0500

    Pagina Administracion al 50%

--- ProyectoX/src/pages/Administrador.jsx
+++ ProyectoX/src/pages/Administrador.jsx
@@ -1,33 +1,75 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import '../Styles/Template.css'
 import { Link  } from 'react-router-dom';
 import {useForm} from 'react-hook-form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import axios from 'axios';
-import { response } from 'express';
+import { faArrowLeft, faWifi, faCar, faWater, faHotTub, faPaw, faUtensils, faSwimmer, faSmokingBan, faUser, faChildReaching } from "@fortawesome/free-solid-svg-icons";
+import { ContextGlobal } from '../components/Services/global.context';
 
 
 
 
 const Administrador = () => {
 
+  const { ciudades, categorias } = useContext(ContextGlobal);
+
+  const atributos = [
+    {
+        id: 0,
+        descripcion: 'Libre de Humo',
+        idIcono: faSmokingBan
+      },
+      {
+        id: 1,
+        descripcion: 'Wifi',
+        idIcono: faWifi
+      },
+      {
+        id: 2,
+        descripcion: 'Restarante',
+        idIcono: faUtensils
+      },
+      {
+        id: 3,
+        descripcion: 'Piscina',
+        idIcono: faSwimmer
+      },
+      {
+        id: 4,
+        descripcion: 'Parqueadero',
+        idIcono: faCar
+      },
+      {
+        id: 5,
+        descripcion: 'Lavanderia',
+        idIcono: faWater
+      },
+      {
+        id: 6,
+        descripcion: 'Jacuzzi',
+        idIcono: faHotTub
+      },
+      {
+        id: 7,
+        descripcion: 'Mascotas',
+        idIcono: faPaw
+      },
+      {
+        id: 8,
+        descripcion: 'Aseo',
+        idIcono: faUser
+      },
+      {
+        id: 9,
+        descripcion: 'Parque Infantil',
+        idIcono: faChildReaching
+      },
+  ];
+
     const { register, formState: { errors } ,handleSubmit } = useForm(); 
 
     const onSubmit = (data) => {
         console.log(data);
-    }
-
-    
-    const ciudades = () => {
-        axios.get ('http://localhost:8080/producto/ciudad/')
-        .then((response) => {
-            console.log(response)
-            this.setState({ciudades: response.data})
-        })
-        .catch((error) => {
-            console.log('error');
-        });
     }
 
 
@@ -56,16 +98,16 @@ const Administrador = () => {
                 <div>
                     <label> Categoria </label>
                     <select {...register('Categoria')}>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                        {categorias.map(categoria => (
+                            <option key={categoria.id} value={categoria.id}> {categoria.titulo} </option>
+                        ))}
                     </select>
                 </div>
                 <div>
                     <label> Ciudad </label>
                     <select {...register('Ciudad')}>
-                        {this.state.ciudades.map(elemento => (
-                            <option key={elemento.id} value={elemento.id}> {elemento.ciudad} </option>
+                        {ciudades.map(ciudad => (
+                            <option key={ciudad.id} value={ciudad.id}> {ciudad.nombre} </option>
                         ))}
                     </select>
                 </div>
@@ -77,6 +119,41 @@ const Administrador = () => {
                     <label> Descripción </label>
                     <input type="textarea" {...register('Descripción')} />
                 </div>
+                <div>
+                    <label> Latitud </label>
+                    <input type="text" {...register('Latitud')} />
+                </div>
+                <div>
+                    <label> Longitud </label>
+                    <input type="text" {...register('Longitud')} />
+                </div>
+                <div>
+                    <label> Caracteristicas </label>
+                    <ul>
+                        {
+                            atributos.map(atributo =>(
+                                <li className="caracteristicasProductosDetail" key={atributo.id}>
+                                    <FontAwesomeIcon icon={atributo.idIcono} />
+                                    <span>{atributo.descripcion}</span>
+                                    <input type="checkbox" name="" id="" />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <label> Normas de la casa </label>
+                    <input type="textarea" {...register('Normas de la casa')} />
+                </div>
+                <div>
+                    <label> Salud y seguridad </label>
+                    <input type="textarea" {...register('Salud y seguridad')} />
+                </div>
+                <div>
+                    <label> Politicas de cancelación </label>
+                    <input type="textarea" {...register('Politicas de cancelación')} />
+                </div>
+                
 
                 <input type= 'submit' value= 'Crear' />
 
