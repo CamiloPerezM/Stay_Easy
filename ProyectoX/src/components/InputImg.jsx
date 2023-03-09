a45278b3859f7792c003fe7ab8259622c3b9f8c7
Mauro9735 <mauriciovilladiego1999@gmail.com>
2023-03-09 13:49:14 -0500

    Cambios finales

--- ProyectoX/src/components/InputImg.jsx
+++ ProyectoX/src/components/InputImg.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus,faX } from '@fortawesome/free-solid-svg-icons';
+import "../Styles/Administrador.css"
 
 function InputImg() {
   const [images, setImages] = useState([""]);
@@ -26,22 +27,22 @@ function InputImg() {
 
   // Renderizar los inputs de imágenes
   const imageInputs = images.map((image, index) => (
-    <div key={index}>
-      <label>{`Imagen ${index + 1}:`}</label>
-      <input
+    <div key={index} className="divImg">
+      <label className="spaceBlocks">{`Imagen ${index + 1}:`}</label>
+      <input className="inputStyleImg"
         type="text"
         value={image}
         onChange={(e) => handleChangeImage(index, e.target.value)}
         required
       />
-      <button type="button" onClick={() => handleRemoveImage(index)}>
+      <button type="button" className="buttonCancel" onClick={() => handleRemoveImage(index)}>
         <FontAwesomeIcon icon={faX}/>
       </button>
     </div>
   ));
 
   // Renderizar el botón de "Agregar Imagen"
-  const addButton = <button type="button" onClick={handleAddImage}><FontAwesomeIcon icon={faPlus}/></button>;
+  const addButton = <button type="button" className="buttonAdd" onClick={handleAddImage}><FontAwesomeIcon icon={faPlus}/></button>;
 
   return (
     <form>
--- ProyectoX/src/pages/Administrador.jsx
+++ ProyectoX/src/pages/Administrador.jsx
@@ -1,74 +1,74 @@
 import React, { useContext } from 'react';
 import '../Styles/Template.css'
 import '../Styles/Administrador.css'
-import { Link  } from 'react-router-dom';
-import {useForm} from 'react-hook-form'
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faWifi, faCar, faWater, faHotTub, faPaw, faUtensils, faSwimmer, faSmokingBan, faUser, faChildReaching } from "@fortawesome/free-solid-svg-icons";
 import { ContextGlobal } from '../components/Services/global.context';
-// import InputImg from '../components/InputImg';
+import InputImg from '../components/InputImg';
 
 
 
 
 const Administrador = () => {
 
-  const { ciudades, categorias } = useContext(ContextGlobal);
+    const { ciudades, categorias } = useContext(ContextGlobal);
 
-  const atributos = [
-    {
-        id: 0,
-        descripcion: 'Libre de Humo',
-        idIcono: faSmokingBan
-      },
-      {
-        id: 1,
-        descripcion: 'Wifi',
-        idIcono: faWifi
-      },
-      {
-        id: 2,
-        descripcion: 'Restarante',
-        idIcono: faUtensils
-      },
-      {
-        id: 3,
-        descripcion: 'Piscina',
-        idIcono: faSwimmer
-      },
-      {
-        id: 4,
-        descripcion: 'Parqueadero',
-        idIcono: faCar
-      },
-      {
-        id: 5,
-        descripcion: 'Lavanderia',
-        idIcono: faWater
-      },
-      {
-        id: 6,
-        descripcion: 'Jacuzzi',
-        idIcono: faHotTub
-      },
-      {
-        id: 7,
-        descripcion: 'Mascotas',
-        idIcono: faPaw
-      },
-      {
-        id: 8,
-        descripcion: 'Aseo',
-        idIcono: faUser
-      },
-      {
-        id: 9,
-        descripcion: 'Parque Infantil',
-        idIcono: faChildReaching
-      },
-  ];
+    const atributos = [
+        {
+            id: 0,
+            descripcion: 'Libre de Humo',
+            idIcono: faSmokingBan
+        },
+        {
+            id: 1,
+            descripcion: 'Wifi',
+            idIcono: faWifi
+        },
+        {
+            id: 2,
+            descripcion: 'Restarante',
+            idIcono: faUtensils
+        },
+        {
+            id: 3,
+            descripcion: 'Piscina',
+            idIcono: faSwimmer
+        },
+        {
+            id: 4,
+            descripcion: 'Parqueadero',
+            idIcono: faCar
+        },
+        {
+            id: 5,
+            descripcion: 'Lavanderia',
+            idIcono: faWater
+        },
+        {
+            id: 6,
+            descripcion: 'Jacuzzi',
+            idIcono: faHotTub
+        },
+        {
+            id: 7,
+            descripcion: 'Mascotas',
+            idIcono: faPaw
+        },
+        {
+            id: 8,
+            descripcion: 'Aseo',
+            idIcono: faUser
+        },
+        {
+            id: 9,
+            descripcion: 'Parque Infantil',
+            idIcono: faChildReaching
+        },
+    ];
 
-    const { register, formState: { errors } ,handleSubmit } = useForm(); 
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
@@ -78,101 +78,107 @@ const Administrador = () => {
     return (
 
         <>
-        <div className='categoryProduct'>
-        <div className="displayFlexColumn">
-                        
-        </div>
-            
-        <Link to="/home"> <span className="arrowLeft"> <FontAwesomeIcon icon={faArrowLeft} /></span> </Link>
-        </div>
-        
+            <div className='categoryProduct'>
+                <div className="displayFlexColumn">
 
-        <section className='fondoCard'>
-            
-            <h3> Crear propiedad </h3>
-            
-            <div>
-        
-            <form onSubmit={handleSubmit (onSubmit)}>
-                <div className='primerBloque'>
-                <div>
-                    <label> Nombre de la propiedad </label>
-                    <input type="text"  {...register('nombrePropiedad', {
-                    require: true, })} className='inputStyle'/>
-                    {errors.nombrePropiedad?.type === 'required' && <p> Este campo no puede estar vacío </p>}
-                </div>
-                <div>
-                    <label> Categoria </label>
-                    <select  {...register('Categoria')} className='inputStyle'>
-                        {categorias.map(categoria => (
-                            <option key={categoria.id} value={categoria.id}> {categoria.titulo} </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label> Ciudad </label>
-                    <select className='inputStyle' {...register('Ciudad')}>
-                        {ciudades.map(ciudad => (
-                            <option key={ciudad.id} value={ciudad.id}> {ciudad.nombre} </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label> Dirección </label>
-                    <input type="text" className='inputStyle' {...register('Dirección')} />
-                </div>
-                <div>
-                    <label> Latitud </label>
-                    <input type="text" className='inputStyle' {...register('Latitud')} />
-                </div>
-                <div>
-                    <label> Longitud </label>
-                    <input type="text" className='inputStyle' {...register('Longitud')} />
-                </div>
-                <div>
-                    <label> Descripción </label>
-                    <input type="textarea" className='inputStylearea' {...register('Descripción')} />
-                </div>
-                </div>
-                <div className='caracteristicas'>
-                <div>
-                    <label className='titulo1'> Caracteristicas </label>
-                    <ul>
-                        {
-                            atributos.map(atributo =>(
-                                <li className="caracteristicasProductosDetail" key={atributo.id}>
-                                    <FontAwesomeIcon icon={atributo.idIcono} />
-                                    <span>{atributo.descripcion}</span>
-                                    <input type="checkbox" name="" id="" className='chequear'/>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-                </div>
-                <div className='adicionales'>
-                <div>
-                    <label> Normas de la casa </label>
-                    <input type="textarea" className='inputStylearea' {...register('Normas de la casa')} />
-                </div>
-                <div>
-                    <label> Salud y seguridad </label>
-                    <input type="textarea" className='inputStylearea' {...register('Salud y seguridad')} />
-                </div>
-                <div>
-                    <label> Politicas de cancelación </label>
-                    <input type="textarea" className='inputStylearea' {...register('Politicas de cancelación')} />
-                </div>
-                </div>
-                <div className='boton'>
-                <input type= 'submit' value= 'Crear' className='envioForm' />
                 </div>
 
-            </form>
-
+                <Link to="/home"> <span className="arrowLeft"> <FontAwesomeIcon icon={faArrowLeft} /></span> </Link>
             </div>
-        </section>
-        
+
+
+            <section className='fondoCard'>
+
+                <h3> Crear propiedad </h3>
+
+                <div>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='primerBloque'>
+                            <div>
+                                <label> Nombre de la propiedad </label>
+                                <input type="text"  {...register('nombrePropiedad', {
+                                    require: true,
+                                })} className='inputStyle' />
+                                {errors.nombrePropiedad?.type === 'required' && <p> Este campo no puede estar vacío </p>}
+                            </div>
+                            <div>
+                                <label> Categoria </label>
+                                <select  {...register('Categoria')} className='inputStyle'>
+                                    {categorias.map(categoria => (
+                                        <option key={categoria.id} value={categoria.id}> {categoria.titulo} </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div>
+                                <label> Ciudad </label>
+                                <select className='inputStyle' {...register('Ciudad')}>
+                                    {ciudades.map(ciudad => (
+                                        <option key={ciudad.id} value={ciudad.id}> {ciudad.nombre} </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div>
+                                <label> Dirección </label>
+                                <input type="text" className='inputStyle' {...register('Dirección')} />
+                            </div>
+                            <div>
+                                <label> Latitud </label>
+                                <input type="text" className='inputStyle' {...register('Latitud')} />
+                            </div>
+                            <div>
+                                <label> Longitud </label>
+                                <input type="text" className='inputStyle' {...register('Longitud')} />
+                            </div>
+                            <div>
+                                <label> Descripción </label>
+                                <input type="textarea" className='inputStylearea' {...register('Descripción')} />
+                            </div>
+                        </div>
+                        <div className='caracteristicas'>
+                            <div>
+                                <label className='titulo1'> Caracteristicas </label>
+                                <ul>
+                                    {
+                                        atributos.map(atributo => (
+                                            <li className="caracteristicasProductosDetail" key={atributo.id}>
+                                                <FontAwesomeIcon icon={atributo.idIcono} />
+                                                <span>{atributo.descripcion}</span>
+                                                <input type="checkbox" name="" id="" className='chequear' />
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                        <label className='titulo1'> Imagenes </label>
+                            <InputImg/>
+                        </div>
+
+                        <div className='adicionales'>
+                            <div>
+                                <label> Normas de la casa </label>
+                                <input type="textarea" className='inputStylearea' {...register('Normas de la casa')} />
+                            </div>
+                            <div>
+                                <label> Salud y seguridad </label>
+                                <input type="textarea" className='inputStylearea' {...register('Salud y seguridad')} />
+                            </div>
+                            <div>
+                                <label> Politicas de cancelación </label>
+                                <input type="textarea" className='inputStylearea' {...register('Politicas de cancelación')} />
+                            </div>
+                        </div>
+                        <div className='boton'>
+                            <Link to="/addedProduct"> <input type='submit' value='Crear' className='envioForm' /></Link>
+                        </div>
+
+                    </form>
+
+                </div>
+            </section>
+
         </>
     )
 
